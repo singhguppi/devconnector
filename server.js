8c75adb8e0e2cb9bf21ef089ba8bc3bbe48771f1
Gurpreet singh <singhguppi1315@gmail.com>
2020-06-15 23:12:50 +0530

    update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const posts = require("./routes/api/posts");
 const profile = require("./routes/api/profile");
 const bodyParser = require("body-parser");
 const passport = require("passport");
-const path = require('path');
+const path = require("path");
 
 //db config
 
@@ -31,15 +31,14 @@ app.use("/api/users", users);
 app.use("/api/posts", posts);
 app.use("/api/profile", profile);
 
-//server static assets if in production
-
+// Server static assets if in production
 if (process.env.NODE_ENV === "production") {
-  //set a static folder
+  // Set static folder
   app.use(express.static("client/build"));
 
-  app.get('*',(res,res)=>{
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
-  })
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 const port = process.env.Port || 3001;
 
